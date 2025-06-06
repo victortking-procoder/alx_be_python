40d6c3e83ae45d08e559acbb5dd50f2668689371
victortking-procoder <fredrickvictor01@outlook.com>
2025-06-06 11:19:38 +0100

    Arithmetic Operations Function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,18 @@
+def perform_operation(num1, num2, operation):
+  match operation:
+    case "add":
+      result = num1 + num2
+      return result
+    case "subtract":
+      result = num1 - num2
+      return result
+    case "multiply":
+      result = num1 * num2
+      return result
+    case "divide":
+      if num2 == 0:
+        print("Not divisible by 0")
+      else:
+        result = num1 / num2
+        return result
+  
