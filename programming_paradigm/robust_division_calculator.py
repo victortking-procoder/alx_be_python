8e588b105d017a04ef9c8162f0dea223783889e3
victortking-procoder <fredrickvictor01@outlook.com>
2025-06-11 13:45:25 +0100

    division_error_calculator

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,10 @@
+def safe_divide(numerator, denominator):
+  try:
+    result = float(numerator) / float(denominator)
+  except ValueError:
+    print("Error: Please enter numeric values only")
+  except ZeroDivisionError:
+    print("Error: Cannot divide by zero.")
+  else:
+    return result
+    
