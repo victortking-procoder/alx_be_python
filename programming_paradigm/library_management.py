d3f02c32f48375963b51cf5daa93a95568bbe578
victortking-procoder <fredrickvictor01@outlook.com>
2025-06-12 12:24:22 +0100

    Basic OOP for a Library Management System

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,41 @@
+class Book:
+  def __init__(self, title, author):
+    self.title = title
+    self.author = author
+    self.__is_checked_out = False
+    
+  def is_checked_out(self):
+    return self.__is_checked_out
+    
+  def check_out(self):
+    self.__is_checked_out = True
+    
+  def return_book(self):
+    self.__is_checked_out = False
+    
+  def __str__(self):
+    return f"{self.title} by {self.author}"
+    
+class Library:
+  def __init__(self):
+    self.__books = []
+  
+  def add_book(self, book):
+    self.__books.append(book)
+    
+  def check_out_book(self, title):
+    for book in self.__books:
+      if title == book.title and not book.is_checked_out():
+        book.check_out()
+        break
+      
+  def return_book(self, title):
+    for book in self.__books:
+      if title == book.title and book.is_checked_out():
+        book.return_book()
+        break
+    
+  def list_available_books(self):
+    for book in self.__books:
+      if not book.is_checked_out():
+        print(book)
