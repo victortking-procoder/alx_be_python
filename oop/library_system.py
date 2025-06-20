e7658a2b48a0369873a701557f716b4b948f421b
victortking-procoder <fredrickvictor01@outlook.com>
2025-06-20 12:12:57 +0100

    Inheritance and Composition in Python

--- oop/library_system.py
+++ oop/library_system.py
@@ -23,11 +23,12 @@ class PrintBook(Book):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
-    books = []
+    def __init__(self):
+        self.books = []
 
     def add_book(self, book):
-        Library.books.append(book)
+        self.books.append(book)
 
     def list_books(self):
-        for book in Library.books:
+        for book in self.books:
             print(book)
